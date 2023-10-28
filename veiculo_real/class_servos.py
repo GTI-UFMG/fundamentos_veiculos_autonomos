986487d0c789ee98afa5e1e0fff97053de43ab46
Armando Alves Neto <64908110+aaneto2@users.noreply.github.com>
2023-10-28 20:52:51 -0300

    Add files via upload

--- veiculo_real/class_servos.py
+++ veiculo_real/class_servos.py
@@ -7,14 +7,18 @@
 # Universidade Federal de Minas Gerais
 ########################################
 import numpy as np
+import time
 from adafruit_servokit import ServoKit
 
 SERVO_STEERING = 0
-SERVO_THROTLE  = 1
+SERVO_THROTTLE  = 1
 
 MAX_STERRING_ANGLE  = 20.0
 ZERO_STERRING_ANGLE = 100.0
 GAIN_STERRING_ANGLE = 50.0/MAX_STERRING_ANGLE
+
+ZERO_THROTTLE_ANGLE = 95.0
+GAIN_THROTTLE_ANGLE = 0.2*90.0
 
 ########################################
 # Adafruit 16-channel servo driver
@@ -22,16 +26,43 @@ GAIN_STERRING_ANGLE = 50.0/MAX_STERRING_ANGLE
 class Servos:
     ########################################
     # construtor
-    def __init__(self):
+    def __init__(self, steering=0.0, throttle=0.0):
         
         # Set channels to the number of servo channels on your kit.
         # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
         self.kit = ServoKit(channels=16)
+        
+        # inicializa o esterçamento
+        self.setSteer(steering)
+        
+        # inicializa traçao
+        self.setU(throttle)
+        
+        # marcha re
+        self.reverse = False
     
     ########################################
     # seta torque do motor
     def setU(self, u):
-        self.kit.servo[SERVO_THROTLE].throttle = u
+        
+        # satura aceleracao
+        u = np.clip(u, -1.0, 1.0)
+        
+        # passar marcha ré
+        if (u < 0.0) and (not self.reverse):
+            self.reverse = True
+            self.backward()
+        if u >=0:
+            self.reverse = False
+            
+        # calibracao
+        u = GAIN_THROTTLE_ANGLE*u + ZERO_THROTTLE_ANGLE
+        
+        # envia comando
+        u = np.clip(u, 0, 180)
+        self.kit.servo[SERVO_THROTTLE].angle = u
+        
+        return
     
     ########################################
     # seta steer do veiculo (st in rad)
@@ -40,19 +71,34 @@ class Servos:
         # converte para graus
         st = np.rad2deg(st)
         
+        # satura angulo de estercamento
+        st = np.clip(st, -MAX_STERRING_ANGLE, MAX_STERRING_ANGLE)
+        
         # aplica calibracao devido a reduções do eixo
         st = GAIN_STERRING_ANGLE*st + ZERO_STERRING_ANGLE
         
         # envia comando
-        st = np.clip(st, 0, 180)
+        st = np.clip(st, 0.0, 180.0)
         self.kit.servo[SERVO_STEERING].angle = st
         
         return
     
     ########################################
+    # mode de marcha ré
+    def backward(self):
+        
+        self.kit.servo[SERVO_THROTTLE].angle = ZERO_STERRING_ANGLE - 20.0
+        time.sleep(0.08)
+        self.kit.servo[SERVO_THROTTLE].angle = ZERO_STERRING_ANGLE
+        time.sleep(0.08)
+        self.kit.servo[SERVO_THROTTLE].angle = ZERO_STERRING_ANGLE - 20.0
+        time.sleep(0.08)
+        self.kit.servo[SERVO_THROTTLE].angle = ZERO_STERRING_ANGLE
+        
+    ########################################
     # destrutor
     def __del__(self):
-        self.kit.deinit()
+        None
         
 ########################################
 # teste
@@ -60,19 +106,15 @@ class Servos:
 import time
 
 atuador = Servos()
-
-#atuador.setSteer(np.deg2rad(0.0))
-#time.sleep(1.0)
-
-'''for i in np.linspace(-20.0, 20.0, 20):
-    print(i)
-    atuador.setSteer(np.deg2rad(i))
-    time.sleep(1.0)'''
     
-    
-for i in np.linspace(1.0, -1.0, 20):
+
+atuador.setU(-1.0)
+time.sleep(3.0)
+
+'''for i in np.linspace(1.0, -1.0, 30):
     print(i)
     atuador.setU(i)
     time.sleep(1.0)
 
 atuador.setU(0.0)
+'''
