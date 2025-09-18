45b821d4c38e30b3182a41fc72e11762cf18d465
Armando Alves Neto <64908110+aaneto2@users.noreply.github.com>
2025-09-18 17:11:31 -0300

    Nova classe do filtro.

--- veiculo_real/class_filter.py
+++ veiculo_real/class_filter.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+########################################
+# Disciplina: Tópicos em Engenharia de Controle e Automação IV (ENG075): 
+# Fundamentos de Veículos Autônomos - 2025/2
+# Professores: Armando Alves Neto e Leonardo A. Mozelli
+# Cursos: Engenharia de Controle e Automação
+# DELT – Escola de Engenharia
+# Universidade Federal de Minas Gerais
+########################################
+
 """
 filters.py — filtros de sinal com herança e API unificada
 
