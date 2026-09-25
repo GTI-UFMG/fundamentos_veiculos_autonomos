a13ab42ae02d18e1dafc6414e8b7a85afb5a2098
Armando Alves Neto <64908110+aaneto2@users.noreply.github.com>
2026-09-25 09:13:19 -0300

    Add files via upload

--- veiculo_real/main.py
+++ veiculo_real/main.py
@@ -106,15 +106,15 @@ if __name__ == "__main__":
 			# telemetria para plots remotos
 			print(
 				f"DATA,"
-				f"{car.t:.3f},"
-				f"{car.p[0]:.3f},"
-				f"{car.p[1]:.3f},"
-				f"{car.v:.3f},"
-				f"{car.vref:.3f},"
-				f"{car.a:.3f},"
-				f"{car.u:.3f},"
-				f"{car.w:.3f},"
-				f"{car.th:.3f}",
+				f"{car.t:.2f},"
+				f"{car.p[0]:.2f},"
+				f"{car.p[1]:.2f},"
+				f"{car.v:.2f},"
+				f"{car.vref:.2f},"
+				f"{car.a:.2f},"
+				f"{car.u:.2f},"
+				f"{car.w:.2f},"
+				f"{car.th:.2f}",
 				flush=True
 			)
 
